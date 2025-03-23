31813e01d7a2bf74f4687a9ca00eda2c9c4735ea
chetnagupta <chetnaguptaa24@gmail.com>
2025-03-23 14:38:59 +0530

    Lazy loading images

--- script.js
+++ script.js
@@ -398,6 +398,38 @@ allSection.forEach(function (section) {
   section.classList.add('section--hidden')
 });
 
+// Lazy Loading images
+
+const imgTarget = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if(!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTarget.forEach(img => imgObserver.observe(img));
+
+
+
+
+
 
 
 
